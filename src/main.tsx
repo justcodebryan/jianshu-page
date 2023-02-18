148cba068c6c43902725017105d29e30c9aa17b1
justcodebryan <huangjifan1819@163.com>
2023-02-18 11:13:10 +0800

    feat: add common components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/iconfont/iconfont'
+import '@/assets/iconfont.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
